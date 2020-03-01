1114403b4d6aa95b1cb1ba5fe3db4e619baaeaca
Unknown <prpr48@naver.com>
2020-03-01 23:11:32 +0900

    add Movie Resolver

---
+++ src/resolvers/MovieResolver.ts
@@ -0,0 +1,10 @@
+import { Resolver, Mutation, Arg } from 'type-graphql';
+
+@Resolver()
+export class MovieResolver {
+  @Mutation(() => Boolean)
+  createMovie(@Arg('title', () => String) title: string) {
+    console.log(title);
+    return true;
+  }
+}
