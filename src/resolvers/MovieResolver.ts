63bdeaf14305f73f80bbad33d2ef66e325434db8
Unknown <prpr48@naver.com>
2020-03-01 23:16:03 +0900

    add Arg minutes

--- src/resolvers/MovieResolver.ts
+++ src/resolvers/MovieResolver.ts
@@ -1,9 +1,12 @@
-import { Resolver, Mutation, Arg } from 'type-graphql';
+import { Resolver, Mutation, Arg, Int } from 'type-graphql';
 
 @Resolver()
 export class MovieResolver {
   @Mutation(() => Boolean)
-  createMovie(@Arg('title', () => String) title: string) {
+  createMovie(
+    @Arg('title') title: string,
+    @Arg('minutes', () => Int) minutes: number
+  ) {
     console.log(title);
     return true;
   }
