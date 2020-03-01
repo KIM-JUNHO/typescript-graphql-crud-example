2df4e03301345b3caf05197209c48b5e018c798a
prpr48 <prpr48@naver.com>
2020-03-02 01:25:28 +0900

    add query

--- src/resolvers/RuleResolver.ts
+++ src/resolvers/RuleResolver.ts
@@ -1,7 +1,12 @@
-import { Resolver, Mutation, Arg } from 'type-graphql';
+import { Resolver, Mutation, Arg, Query } from 'type-graphql';
 
 @Resolver()
 export class RuleResolver {
+  @Query(() => String)
+  hello() {
+    return 'hi!';
+  }
+
   @Mutation(() => Boolean)
   createRule(@Arg('srcAddr') srcAddr: string, @Arg('dstAddr') dstAddr: string) {
     console.log(srcAddr, dstAddr);
